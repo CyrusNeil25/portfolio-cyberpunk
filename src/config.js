c7707e59e38b94dc36e43ea612d0347527aeca4d
CyrusNeil25 <shubz493@gmail.com>
2026-06-24 22:33:47 +0530

    Image Routes

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@
 // Avatar: drop your image/gif into the public/ folder and set the filename here.
 // Examples: '/avatar.jpg'  '/avatar.png'  '/avatar.gif'  '/me.webp'
 // Set to null to keep the animated placeholder.
-export const AVATAR_SRC = '/Apex.gif';
+export const AVATAR_SRC = `${import.meta.env.BASE_URL}images/profile.png`;
 
 // Your display name shown in the About sections
 export const YOUR_NAME = 'Cyrus Neil';
